0dc1c1385e5a75a57088e2a69b0efcad8d81fb86
Pooja Unnikrishnan <poojaunnikrishnan7558@gmailcom>
2024-10-18 16:26:56 +0530

    Updated sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,31 +1,55 @@
-import { data } from '../Data/data'
-export const Sidebar = ({ filters, updateFilters, setFilters }) => {
-    return (
-        <div className='mx-4 p-8 shadow-lg mt-28'>
-            {data.clothingOptions.map((clothingItem) => (
-                <div className='py-3' key={clothingItem.value}>
-                    <div className='font-bold'>{clothingItem.value}</div>
-                    <ul className='py-2'>
-                        {clothingItem.options.map((item, i) => (
-                            <li className='flex items-center gap-4' key={i} onClick={() => updateFilters(clothingItem.value, item)}>
-                                <svg
-                                    width="12"
-                                    height="12"
-                                    viewBox="0 0 12 12"
-                                    fill={filters[clothingItem.value]?.[item] ? "#000" : "#C4C4C4"}
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <rect width="12" height="12" />
-                                    {filters[clothingItem.value]?.[item] && (
-                                        <path d="M2 6 L5 9 L10 2" stroke="white" strokeWidth="1" fill="none" />
-                                    )}
-                                </svg>
-                                {item}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+export const Sidebar = ({
+  filters,
+  updateFilters,
+  products,
+  priceRanges,
+  className,
+}) => {
+  const extractUniqueValues = (key) =>
+    Array.from(new Set(products.map((item) => item[key])));
+  const filterCategories = [
+    { name: "Gender", values: extractUniqueValues("gender") },
+    { name: "Color", values: extractUniqueValues("color") },
+    { name: "Type", values: extractUniqueValues("type") },
+    { name: "Price", values: priceRanges.map((range) => range.name) },
+  ];
+
+  return (
+    <div class={`w-full shadow-lg bg-slate-900 text-gray-300 ${className}`}>
+      {filterCategories.map((category) => (
+        <div class="py-3" key={category.name}>
+          <span class="sr-only">Filters</span>
+          <div class="font-bold text-white">{category.name}</div>
+          <ul class="py-2">
+            {category.values.map((value, i) => (
+              <li
+                class="flex items-center gap-4"
+                key={i}
+                onClick={() => updateFilters(category.name, value)}
+              >
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="#C4C4C4"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width="12" height="12" />
+                  {filters[category.name]?.[value] && (
+                    <path
+                      d="M2 6 L5 9 L10 2"
+                      stroke="black"
+                      strokeWidth="2"
+                      fill="none"
+                    />
+                  )}
+                </svg>
+                {value}
+              </li>
             ))}
+          </ul>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
